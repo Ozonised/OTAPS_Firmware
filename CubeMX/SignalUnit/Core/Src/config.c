168e76c4294fcbbb02ce050c493aea46e6949789
Farhan Khan <74808685+Ozonised@users.noreply.github.com>
2024-10-13 21:36:47 +0530

    feat: add node IDs to config file

--- CubeMX/SignalUnit/Core/Src/config.c
+++ CubeMX/SignalUnit/Core/Src/config.c
@@ -13,5 +13,12 @@ const uint8_t TOTAL_NO_OF_NODES = 5;
 // Node addresses
 const uint8_t THIS_NODE_ADDRESS[5] = "NRF14";
 const uint8_t PREV_NODE_ADDRESS[5] = "NRF14";
+const uint8_t NEXT_NODE_ADDRESS[5] = "NRF14";
 const uint8_t LOCOMOTIVE_NODE_ADDRESS[5] = "LOCOT";
+
+// Node ID
+// for the sake of simplicity, node number will the node ID
+const uint8_t THIS_NODE_ID = THIS_NODE_NUM;
+const uint8_t PREV_NODE_ID = THIS_NODE_NUM - 1;
+const uint8_t NEXT_NODE_ID = THIS_NODE_NUM + 1;
 
